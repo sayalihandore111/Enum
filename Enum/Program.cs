f819f363231288a53197a9940d94d3ba97fd586a
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 23:39:39 +0530

    Add project files.

---
+++ Enum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    class Program
+    {
+        enum MeetingImportance
+        {
+            Trivial,
+            Regular,
+            Critical,
+        }
+        static void Main(string[] args)
+        {
+            MeetingImportance meet = MeetingImportance.Critical;
+            int value = (int)MeetingImportance.Critical;
+
+            if (meet == MeetingImportance.Critical)
+            {
+                Console.WriteLine("Critical " + value);
+            }
+            else if (meet == MeetingImportance.Trivial)
+            {
+                Console.WriteLine("Trivial " + value);
+            }
+
+            else if (meet == MeetingImportance.Regular)
+            {
+                Console.WriteLine("Regular " + value);
+            }
+        }
+    }
+}
